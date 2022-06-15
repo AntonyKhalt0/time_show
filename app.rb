df7f2a141183160e7f17f657e24c9ca711f83fe6
AntonyKhalt0 <toscha.xalturin@gmail.com>
2022-06-15 19:44:50 +0300

    shared the app and added format_time.rb

--- app.rb
+++ app.rb
@@ -1,47 +1,24 @@
-class App
-  DATE_TIME = { 'year' => Time.now.year, 
-                'month' => Time.now.month, 
-                'day' => Time.now.day, 
-                'hour' => Time.now.hour, 
-                'minute' => Time.now.min,
-                'second' => Time.now.sec }.freeze
+require_relative 'format_time'
 
+class App
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    path_definition(req, resp)
+    response_body(req, resp)
       
     resp.finish
   end
 
   private
 
-  def path_definition(req, resp)
-    if req.path.match(/time/)
-      array_params_create(req, resp)
+  def response_body(req, resp)
+    formatter = FormatTime.new(req, resp)
+    if formatter.valid?
+      formatter.valid_formats
     else
-      resp.status = 404
+      formatter.invalid_formats
     end
   end
 
-  def array_params_create(req, resp)
-    array_params = req.params['format'].split(/,/)
-    valid_date = []
-    invalid_date = []
-    array_params.each do |time|
-      if DATE_TIME.keys.include?time
-        valid_date.push("#{DATE_TIME[time]}")
-      else
-        invalid_date.push(time)
-      end
-    end
-    if invalid_date.any?
-      resp.status = 404
-      resp.write "Unknown time format #{invalid_date}"
-    else
-      resp.write valid_date.join("-")
-    end
-    resp
-  end
 end
